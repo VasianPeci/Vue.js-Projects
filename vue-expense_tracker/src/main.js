18439c34ad9abc1ae298fd56a4e86b306bc7edb1
VasianPeci <140719484+VasianPeci@users.noreply.github.com>
2025-08-13 16:57:56 +0200

    Delete vue-expense_tracker directory

--- vue-expense_tracker/src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/style.css'
-import Toast from 'vue-toastification';
-import 'vue-toastification/dist/index.css';
-import { createApp } from 'vue'
-import App from './App.vue'
-
-const app = createApp(App);
-app.use(Toast);
-app.mount('#app')
